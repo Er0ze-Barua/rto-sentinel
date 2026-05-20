49c298759d8953664748cb50b035f66ec33e1009
Er0ze-Barua <beroze182@gmail.com>
2026-05-20 13:58:38 +0530

    fix: CSRF trusted origins and disable DEBUG

--- rto_web/settings.py
+++ rto_web/settings.py
@@ -27,7 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['rto-sentinel-frontend.onrender.com', 'localhost', '127.0.0.1']
 
-
+CSRF_TRUSTED_ORIGINS = ['https://rto-sentinel-frontend.onrender.com']
+DEBUG = False
 # Application definition
 
 INSTALLED_APPS = [
